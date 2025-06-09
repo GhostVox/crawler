7f92729217422d247e9d5505b8be6b7491899dc0
Brent-the-carpenter <beh337@gmail.com>
2025-06-08 22:08:08 -0400

    Finished the project

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	u "net/url"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 	"sync"
 )
@@ -17,6 +19,7 @@ type config struct {
 	mu                 *sync.Mutex
 	concurrencyControl chan struct{}
 	wg                 *sync.WaitGroup
+	maxPages           int
 }
 
 func main() {
@@ -25,7 +28,7 @@ func main() {
 		println("no website provided\r\nusage: ./go run main.go <URL>")
 		os.Exit(1)
 	}
-	if len(args) > 1 {
+	if len(args) > 3 {
 		println("too many arguments provided, only one URL is expected")
 		os.Exit(1)
 	}
@@ -40,15 +43,30 @@ func main() {
 		mu:                 &sync.Mutex{},
 		concurrencyControl: make(chan struct{}, 100), // limit concurrency to 10
 		wg:                 &sync.WaitGroup{},
+		maxPages:           1,
+	}
+	if len(args) >= 2 {
+		maxPages, err := strconv.Atoi(args[2])
+		if err != nil {
+			println("error parsing max pages:", err.Error())
+			os.Exit(1)
+		}
+		cfg.maxPages = maxPages
+	}
+	if len(args) == 3 {
+		maxConcurrency, err := strconv.Atoi(args[1])
+		if err != nil {
+			println("error parsing max concurrency:", err.Error())
+			os.Exit(1)
+		}
+		cfg.concurrencyControl = make(chan struct{}, maxConcurrency)
 	}
 	cfg.wg.Add(1)
 	go cfg.crawlPage(args[0])
 
 	cfg.wg.Wait()
 	fmt.Println("\nCrawling completed. Found URLs:")
-	for url, count := range cfg.pages {
-		fmt.Printf("%s (found %d times)\n", url, count)
-	}
+	cfg.printReport(args[0])
 
 }
 func getHTML(rawURL string) (string, error) {
@@ -99,6 +117,10 @@ func (cfg *config) crawlPage(rawCurrentURL string) {
 			return
 		}
 		for _, url := range urls {
+			if len(cfg.pages) >= cfg.maxPages {
+				fmt.Println("max pages reached, stopping crawl")
+				return
+			}
 			cfg.wg.Add(1)
 			go cfg.crawlPage(url)
 		}
@@ -115,4 +137,31 @@ func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
 	}
 	cfg.pages[normalizedURL] = 1
 	return true
+}
+
+func (cfg *config) printReport(baseUrl string) {
+	fmt.Printf("=============================REPORT for %v\n=============================\r\n", baseUrl)
+	// Convert map to slice for sorting
+	type urlCount struct {
+		url   string
+		count int
+	}
+
+	var urls []urlCount
+	for url, count := range cfg.pages {
+		urls = append(urls, urlCount{url, count})
+	}
+
+	// Sort by count (desc), then alphabetically
+	sort.Slice(urls, func(i, j int) bool {
+		if urls[i].count == urls[j].count {
+			return urls[i].url < urls[j].url
+		}
+		return urls[i].count > urls[j].count
+	})
+
+	// Print results
+	for _, u := range urls {
+		fmt.Printf("Found %d internal links to %s", u.count, u.url)
+	}
 }
